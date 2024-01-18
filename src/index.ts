c1a96bae5fb358e6f38baa0f5894cd6100e9a693
shalev <shalevari2008@gmail.com>
2024-01-18 19:57:47 +0200

    refactor: concise functions

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,43 @@
-import { json } from "body-parser";
-import cookieParser from "cookie-parser";
-import "dotenv/config";
-import express from "express";
-import { createServer } from "http";
-import mongoose from "mongoose";
+import {json} from "body-parser"
+import cookieParser from "cookie-parser"
+import "dotenv/config"
+import express from "express"
+import {createServer} from "http"
+import mongoose from "mongoose"
 
-const app = express();
+const app = express()
 
-app.use(cookieParser());
-app.use(json());
+app.use(cookieParser())
+app.use(json())
 
 app.get("/api/hello", (req, res) => {
-	res.send("world");
-});
+	res.send("world")
+})
 
-app.use(express.static("public"));
+app.use(express.static("public"))
 
-const server = createServer(app);
-const port = process.env.PORT ?? 3000;
+const server = createServer(app)
+const port = process.env.PORT ?? 3000
 
 async function init() {
+	checkMongoConnectionString()
+	listen()
+}
+
+async function checkMongoConnectionString() {
 	if (!process.env.MONGO_CONNECTION_STRING) {
-		throw new Error("Must provide connection string for mongodb");
+		throw new Error("No MongoDB connection string")
 	}
 
 	await mongoose.connect(process.env.MONGO_CONNECTION_STRING, {
 		dbName: "bullet-serve-app",
-	});
-
-	server.listen(port, () =>
-		console.log(`Listening on http://localhost:${port}`)
-	);
+	})
 }
 
-init();
+async function listen() {
+	server.listen(port, () =>
+		console.log(`Listening on http://localhost:${port}`)
+	)
+}
+
+init()
