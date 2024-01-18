98638c857462bc06427417ef40ff56d203c094bf
shalev <shalevari2008@gmail.com>
2024-01-18 20:04:13 +0200

    style: prettier config

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,43 @@
-import {json} from "body-parser"
-import cookieParser from "cookie-parser"
-import "dotenv/config"
-import express from "express"
-import {createServer} from "http"
-import mongoose from "mongoose"
+import { json } from 'body-parser'
+import cookieParser from 'cookie-parser'
+import 'dotenv/config'
+import express from 'express'
+import { createServer } from 'http'
+import mongoose from 'mongoose'
 
 const app = express()
 
 app.use(cookieParser())
 app.use(json())
 
-app.get("/api/hello", (req, res) => {
-	res.send("world")
+app.get('/api/hello', (req, res) => {
+  res.send('world')
 })
 
-app.use(express.static("public"))
+app.use(express.static('public'))
 
 const server = createServer(app)
 const port = process.env.PORT ?? 3000
 
 async function init() {
-	checkMongoConnectionString()
-	listen()
+  checkMongoConnectionString()
+  listen()
 }
 
 async function checkMongoConnectionString() {
-	if (!process.env.MONGO_CONNECTION_STRING) {
-		throw new Error("No MongoDB connection string")
-	}
+  if (!process.env.MONGO_CONNECTION_STRING) {
+    throw new Error('No MongoDB connection string')
+  }
 
-	await mongoose.connect(process.env.MONGO_CONNECTION_STRING, {
-		dbName: "bullet-serve-app",
-	})
+  await mongoose.connect(process.env.MONGO_CONNECTION_STRING, {
+    dbName: 'bullet-serve-app',
+  })
 }
 
 async function listen() {
-	server.listen(port, () =>
-		console.log(`Listening on http://localhost:${port}`)
-	)
+  server.listen(port, () =>
+    console.log(`Listening on http://localhost:${port}`)
+  )
 }
 
 init()
--- src/models.ts
+++ src/models.ts
@@ -1,26 +1,26 @@
-type State = "ordered" | "approved" | "kitchen" | "paid" | "delivery";
-type Resolution = "completed" | "declined" | "aborted";
-type Role = "admin" | "kitchen" | "cashier" | "delivery";
+type State = 'ordered' | 'approved' | 'kitchen' | 'paid' | 'delivery'
+type Resolution = 'completed' | 'declined' | 'aborted'
+type Role = 'admin' | 'kitchen' | 'cashier' | 'delivery'
 
 interface Order {
-	orderID: string;
-	state: State;
-	items: string[];
-	price: number;
-	clientNotes: string;
-	resolution: Resolution;
-	orderDate: Date;
+  orderID: string
+  state: State
+  items: string[]
+  price: number
+  clientNotes: string
+  resolution: Resolution
+  orderDate: Date
 }
 
 interface EmployeeUser {
-	userName: string;
-	password: string;
-	role: Role;
+  userName: string
+  password: string
+  role: Role
 }
 
 interface CustomerUser {
-	userName: string;
-	password: string;
-	phone: string;
-	orders: Order[];
+  userName: string
+  password: string
+  phone: string
+  orders: Order[]
 }
