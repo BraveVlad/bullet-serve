cbe6a6543840f974e80f6b87f9bae4f15967043b
BraveVlad <giladvladpinker@gmail.com>
2024-01-22 17:57:12 +0200

    feat(back): requests logger middleware

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,48 @@
-import { json } from 'body-parser'
-import cookieParser from 'cookie-parser'
-import 'dotenv/config'
-import express from 'express'
-import { createServer } from 'http'
-import mongoose from 'mongoose'
-import { router as orderRouter } from './Order.router'
+import { json } from 'body-parser';
+import cookieParser from 'cookie-parser';
+import 'dotenv/config';
+import express from 'express';
+import { createServer } from 'http';
+import mongoose from 'mongoose';
+import { router as orderRouter } from './Order.router';
+import { RequestHandler } from 'express-serve-static-core';
 
-const app = express()
+const app = express();
 
-app.use(cookieParser())
-app.use(json())
+app.use(cookieParser());
+app.use(json());
 
-app.use('/api/orders', orderRouter)
+const logRequests: RequestHandler = (req, res, next) => {
+  console.log(req.method, req.url, req.body);
+  next();
+};
 
-app.use(express.static('public'))
+app.use('/api/orders', orderRouter);
 
-const server = createServer(app)
-const port = process.env.PORT ?? 3000
+app.use(express.static('public'));
+
+const server = createServer(app);
+const port = process.env.PORT ?? 3000;
 
 async function init() {
-  checkMongoConnectionString()
-  listen()
+  checkMongoConnectionString();
+  listen();
 }
 
 async function checkMongoConnectionString() {
   if (!process.env.MONGO_CONNECTION_STRING) {
-    throw new Error('No MongoDB connection string')
+    throw new Error('No MongoDB connection string');
   }
 
   await mongoose.connect(process.env.MONGO_CONNECTION_STRING, {
     dbName: 'bullet-serve-app',
-  })
+  });
 }
 
 async function listen() {
   server.listen(port, () =>
     console.log(`Listening on http://localhost:${port}`)
-  )
+  );
 }
 
-init()
+init();
