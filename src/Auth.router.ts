b22e5038737886d018c1cdc209a1adc2b3c8a182
BraveVlad <giladvladpinker@gmail.com>
2024-01-22 19:45:19 +0200

    feat(back): Added customer / employee register/login

--- src/Auth.router.ts
+++ src/Auth.router.ts
@@ -1,3 +1,126 @@
 import { Router } from 'express';
+import { Customer, Employee } from './Users.model';
 
 export const router = Router();
+
+router.post('/employee/register', async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+
+    if (
+      !username ||
+      !password ||
+      typeof username !== 'string' ||
+      typeof password !== 'string'
+    ) {
+      res.status(400);
+      res.send('must provide username and password');
+    }
+
+    const validationError = validateCredentials(username, password);
+    if (validationError) {
+      res.status(400);
+      res.send(validationError);
+      return;
+    }
+
+    await Employee.create({ username, password });
+
+    res.status(201).end();
+  } catch (err) {
+    if (typeof err === 'object' && err && 'code' in err && err.code === 11000) {
+      res.status(409).send('username already taken');
+      return;
+    }
+    next(err);
+  }
+});
+
+router.post('/customer/register', async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+
+    if (
+      !username ||
+      !password ||
+      typeof username !== 'string' ||
+      typeof password !== 'string'
+    ) {
+      res.status(400);
+      res.send('must provide username and password');
+    }
+
+    const validationError = validateCredentials(username, password);
+    if (validationError) {
+      res.status(400);
+      res.send(validationError);
+      return;
+    }
+
+    await Customer.create({ username, password });
+
+    res.status(201).end();
+  } catch (err) {
+    if (typeof err === 'object' && err && 'code' in err && err.code === 11000) {
+      res.status(409).send('username already taken');
+      return;
+    }
+    next(err);
+  }
+});
+
+router.post('/employee/login', async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+
+    const employee = await Employee.findOne({
+      username,
+      password,
+    });
+
+    if (!employee) {
+      res.status(401).send("username or password doesn't match.");
+      return;
+    }
+
+    res.cookie('employeeId', employee._id, {
+      signed: true,
+      secure: true,
+      httpOnly: true,
+    });
+
+    res.status(200).send();
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.post('/customer/login', async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+
+    const customer = await Customer.findOne({
+      username,
+      password,
+    });
+
+    if (!customer) {
+      res.status(401).send("username or password doesn't match.");
+      return;
+    }
+
+    res.cookie('customerId', customer._id, {
+      signed: true,
+      secure: true,
+      httpOnly: true,
+    });
+
+    res.status(200).send();
+  } catch (err) {
+    next(err);
+  }
+});
+
+export function validateCredentials(username: string, password: string) {
+  return undefined;
+}
--- src/Users.model.ts
+++ src/Users.model.ts
@@ -1,26 +1,26 @@
-import { Schema, model } from 'mongoose'
-import { Order, orderSchema } from './Order.model'
-import { createHash } from 'crypto'
+import { Schema, model } from 'mongoose';
+import { Order, orderSchema } from './Order.model';
+import { createHash } from 'crypto';
 
-const EMOPLOYEE_ROLE = ['ADMIN', 'KITCHEN', 'CASHIER', 'DELIVERY']
+const EMOPLOYEE_ROLE = ['ADMIN', 'KITCHEN', 'CASHIER', 'DELIVERY'];
 
 export interface Employee {
-  username: string
-  password: string
-  role: (typeof EMOPLOYEE_ROLE)[number]
+  username: string;
+  password: string;
+  role: (typeof EMOPLOYEE_ROLE)[number];
 }
 
 export const employeeSchema = new Schema<Employee>({
   username: { type: String, unique: true },
   password: { type: String, set: hashPassword },
   role: EMOPLOYEE_ROLE,
-})
+});
 
 interface Customer {
-  username: string
-  password: string
-  phone: string
-  orders: Order[]
+  username: string;
+  password: string;
+  phone: string;
+  orders: Order[];
 }
 
 export const customerSchema = new Schema<Customer>({
@@ -28,14 +28,22 @@ export const customerSchema = new Schema<Customer>({
   password: { type: String, set: hashPassword },
   phone: { type: String, unique: true },
   orders: [orderSchema],
-})
+});
 
 export function hashPassword(value: string) {
-  const hash = createHash('sha256')
-  hash.update(value)
+  const hash = createHash('sha256');
+  hash.update(value);
 
-  return hash.digest('base64')
+  return hash.digest('base64');
 }
 
-export const Customer = model<Customer>('Customer', customerSchema, 'customers')
-export const Employee = model<Employee>('Employee', employeeSchema, 'employees')
+export const Customer = model<Customer>(
+  'Customer',
+  customerSchema,
+  'customers'
+);
+export const Employee = model<Employee>(
+  'Employee',
+  employeeSchema,
+  'employees'
+);
