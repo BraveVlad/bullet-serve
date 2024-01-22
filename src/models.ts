d31a9e201e6963315a0846b061f2e77a777b6f0f
BraveVlad <giladvladpinker@gmail.com>
2024-01-22 14:35:32 +0200

    feat(back): added model schemas and defined models

--- src/models.ts
+++ src/models.ts
@@ -1,4 +1,4 @@
-import { Schema } from 'mongoose'
+import { Schema, model } from 'mongoose'
 
 const ORDER_STATE = [
   ,
@@ -19,19 +19,46 @@ export interface Order {
   items: string[]
   price: number
   clientNotes: string
-  resolution: Resolution
+  resolution: (typeof EMOPLOYEE_ROLE)[number]
   orderDate: Date
 }
 
-interface Employee {
-  userName: string
+export const orderSchema = new Schema<Order>({
+  orderID: String,
+  state: ORDER_STATE,
+  items: [String],
+  price: Number,
+  clientNotes: String,
+  resolution: RESOLUTION,
+  orderDate: Date,
+})
+
+export interface Employee {
+  username: string
   password: string
-  role: EmployeeRole
+  role: (typeof EMOPLOYEE_ROLE)[number]
 }
 
+export const employeeSchema = new Schema<Employee>({
+  username: String,
+  password: String,
+  role: EMOPLOYEE_ROLE,
+})
+
 interface Customer {
-  userName: string
+  username: string
   password: string
   phone: string
   orders: Order[]
 }
+
+export const customerSchema = new Schema<Customer>({
+  username: String,
+  password: String,
+  phone: String,
+  orders: [orderSchema],
+})
+
+export const Order = model<Order>('Order', orderSchema, 'orders')
+export const Customer = model<Customer>('Customer', customerSchema, 'customers')
+export const Employee = model<Employee>('Employee', employeeSchema, 'employees')
